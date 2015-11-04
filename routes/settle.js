b0c61b202a9a36ec24dd757e71ac9362ec6b2772
trainerbill <athroener@mystartupsolutions.com>
2015-11-04 13:06:21 -0600

    Update settle.js

--- routes/settle.js
+++ routes/settle.js
@@ -2,6 +2,7 @@ module.exports = function (app) {
      app.post('/process/settle', function (req, res, next) {
         var gateway = app.get('btgateway');
         var form = req.body;
+        console.log(form);
         gateway.transaction.submitForSettlement(form.trxid, function (err, result) {
             if (err) {
                 res.send(err);
